3028952e1a601cdc5d4498dd5a8ee42c8e8b0729
hugethekat <117039840+hugethekat@users.noreply.github.com>
2024-04-09 01:07:24 -0700

    Spring Configuration
    eitale

--- moodle/src/main/java/com/itson/hired/demo/MoodleApplication.java
+++ moodle/src/main/java/com/itson/hired/demo/MoodleApplication.java
@@ -2,8 +2,10 @@ package com.itson.hired.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.itson.hired.demo.config")
 public class MoodleApplication {
 
 	public static void main(String[] args) {
